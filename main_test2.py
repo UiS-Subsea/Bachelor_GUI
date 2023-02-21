3f4c2f7912f3365ff3ad82b255579e315f6cab17
domas7 <petniunas1@gmail.com>
2023-02-21 10:57:16 +0100

    fixed errors

--- main_test2.py
+++ main_test2.py
@@ -3,6 +3,19 @@ import threading
 from network_handler import Network
 import json
 from Thread_info import Threadwatcher
+import multiprocessing
+import time
+
+
+class Rov_state:
+    def __init__(self, queue, network_handler, gui_pipe, t_watch: Threadwatcher) -> None:
+        self.t_watch: Threadwatcher = t_watch
+        self.data: dict = {}
+        self.queue: multiprocessing.Queue = queue
+        # Pipe to send sensordata back to the gui
+        self.gui_pipe = gui_pipe
+        # Network handler that sends data to rov (and recieves)
+        self.network_handler: Network = network_handler
 
 
 def recieve_data_from_rov(self, network: Network, t_watch: Threadwatcher, id: int):
@@ -36,9 +49,9 @@ def decode_packets(tcp_data: bytes, end_not_complete_packet="") -> list:
     try:
         json_strings = end_not_complete_packet+bytes.decode(tcp_data, "utf-8")
         print(json_strings)
-        # pakken er ikke hel. Dette skal aldri skje så pakken burde bli forkasta
+        # pakken er ikke hel. Dette skal aldri skje sÃ¥ pakken burde bli forkasta
         if not json_strings.startswith('"*"'):
-            #print(f"Packet did not start with '*' something is wrong. {end_not_complete_packet}")
+            # print(f"Packet did not start with '*' something is wrong. {end_not_complete_packet}")
             return [], ""
         if not json_strings.endswith('"*"'):  # pakken er ikke hel
             end_not_complete_packet = json_strings[json_strings.rfind("*")-1:]
@@ -97,59 +110,64 @@ def handle_data_from_rov(self, message: dict):
         print(f"\n\nMESSAGE NOT RECOGNISED\n{message}\n")
 
 
-def craft_packet(self, t_watch: Threadwatcher, id):
-    while t_watch.should_run(id):
-        userinput = input(
-            "Packet: [parameter_id of type int, value of type float or int]: ")
-        var = []
-        try:
-            var = json.loads(userinput)
-            if not isinstance(var[0], int):
-                print("Error: parameter id was not an int! try again.")
-                continue
-            # if not isinstance(var[1], int) or not isinstance(var[1], float):
-            #     print("Error: parameter id was not an int or float! try again.")
-            #     continue
-            if len(var) != 2:
-                print("Error: list was not length 2")
-                continue
-        except Exception as e:
-            print(f"Error when parsing input\n {e}")
-            continue
-
-        self.packets_to_send.append([ID_DIRECTIONCOMMAND_PARAMETERS, var])
+def network_format(data) -> bytes:
+    """Formats the data for sending to network handler"""
+    packet_seperator = json.dumps("*")
+    return bytes(packet_seperator+json.dumps(data)+packet_seperator, "utf-8")
 
 
-def send_packets(self):
-    """Sends the created network packets and clears it"""
-
-    copied_packets = self.packets_to_send
-    self.packets_to_send = []
-    for packet in copied_packets:
-        if packet[0] != ID_DIRECTIONCOMMAND:
-            pass
-            print(f"{packet = }")
-    if run_network:
-        self.logger.sensor_logger.info(copied_packets)
-    if self.network_handler is None:
-        return
-    self.network_handler.send(network_format(copied_packets))
+def run(network_handler: Network, t_watch: Threadwatcher, id: int, queue_for_rov: multiprocessing.Queue, gui_pipe):
+    print(f"{network_handler = }")
+    rov_state = Rov_state(queue_for_rov, network_handler, gui_pipe, t_watch)
+    if network_handler != None:
+        id = t_watch.add_thread()
+        threading.Thread(target=rov_state.recieve_data_from_rov, args=(
+            network_handler, t_watch, id), daemon=True).start()
 
 
 if __name__ == "__main__":
 
-    global run_network
-    run_network = False
+    try:
+        global network
+        run_network = False
 
-    network = None
-    if run_network:
-        network = Network(is_server=False, port=6900, connect_addr="10.0.0.2")
-        print("network started")
+        run_gui = True
+        run_get_controllerdata = False
+        run_network = False
+        run_craft_packet = True
+        run_send_fake_sensordata = True
+        manual_input_rotation = False
+
+        queue_for_rov = multiprocessing.Queue()
+
         t_watch = Threadwatcher()
+
+        # starts the gui program. gui_parent_pipe should get the sensor data
+        # starts the gui program. gui_parent_pipe should get the sensor data
+        gui_parent_pipe, gui_child_pipe = Pipe()
+
+        network = None
+        if run_network:
+            network = Network(is_server=False, port=6900,
+                              connect_addr="10.0.0.2")
+            print("network started")
 
         print("starting send to rov")
         id = t_watch.add_thread()
+        print(id)
+        main_driver_loop = threading.Thread(target=run, args=(
+            network, t_watch, id, queue_for_rov, gui_parent_pipe), daemon=True)
+        main_driver_loop.start()
 
-    main_driver_loop = threading.Thread(target=run, args=(
-        network, t_watch, id, queue_for_rov, gui_parent_pipe), daemon=True)
-    main_driver_loop.start()
+        """
+        main_driver_loop = threading.Thread(target=run, args=(
+            network, t_watch, id, queue_for_rov, gui_parent_pipe), daemon=True)
+        main_driver_loop.start() """
+
+        # dummy_data1 = json.loads(r'[{"""" id": 1}, {"id": 2}]')
+
+        while True:
+            time.sleep(5)
+    except KeyboardInterrupt:
+        t_watch.stop_all_threads()
+        print("stopped all threads")
