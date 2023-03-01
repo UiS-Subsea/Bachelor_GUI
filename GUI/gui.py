636b2103e51223fd8c255f03c7172efc28de3146
martindimmen <martindimmen@proton.me>
2023-03-01 23:45:39 +0100

    no e det kaos her

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,24 +1,27 @@
-import sys
 from PyQt6 import QtWidgets, uic
-from pyqtgraph import PlotWidget
+from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
-
+import sys  # We need sys so that we can pass argv to QApplication
+import os
 
 class MainWindow(QtWidgets.QMainWindow):
-    def __init(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-        uic.loadUi("mainwindow.ui", self)
 
-        self.show()
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        #Load the UI Page
+        uic.loadUi('mainwindow.ui', self)
 
         self.plot([1,2,3,4,5,6,7,8,9,10], [30,32,34,32,33,31,29,32,35,45])
-        
-        def plot(self, hour, temperature):
-            self.graphWidget.plot(hour, temperature)
 
+    def plot(self, hour, temperature):
+        self.graphWidget.plot(hour, temperature)
 
-app = QtWidgets.QApplication(sys.argv)
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
 
-window = uic.loadUi("mainwindow.ui")
-window.show()
-app.exec()
+if __name__ == '__main__':
+    main()
