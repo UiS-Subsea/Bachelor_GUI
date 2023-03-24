5e9a9af8346a2e1a8dc498c6aff0eaaf845c0cc0
domas7 <petniunas1@gmail.com>
2023-03-24 13:36:23 +0100

    Fixed fuse_reset function uses 0/1 to reset

--- Kommunikasjon/test.py
+++ Kommunikasjon/test.py
@@ -60,26 +60,41 @@
 # if __name__ == "__main__":
 #     r = reset_12V_thruster_fuse(1)
 #     print(r)
-regulator_active: list[bool] = [True, True, True]
-packets_to_send = []
-X_AXIS = 1
-Y_AXIS = 0
-Z_AXIS = 6
-ROTATION_AXIS = 2
-data = {}
+# regulator_active: list[bool] = [True, True, True]
+# packets_to_send = []
+# X_AXIS = 1
+# Y_AXIS = 0
+# Z_AXIS = 6
+# ROTATION_AXIS = 2
+# data = {}
 
 
-def build_rov_packet():
-    data = [0, 0, 0, 0, 0, 0, 0, 0]
-    data[0] = data["rov_joysticks"][X_AXIS]
-    data[1] = data["rov_joysticks"][Y_AXIS]
-    data[2] = data["rov_joysticks"][Z_AXIS]
-    data[3] = data["rov_joysticks"][ROTATION_AXIS]
-    packets_to_send.append([40, data])
+# def build_rov_packet():
+#     data = [0, 0, 0, 0, 0, 0, 0, 0]
+#     data[0] = data["rov_joysticks"][X_AXIS]
+#     data[1] = data["rov_joysticks"][Y_AXIS]
+#     data[2] = data["rov_joysticks"][Z_AXIS]
+#     data[3] = data["rov_joysticks"][ROTATION_AXIS]
+#     packets_to_send.append([40, data])
+#     print(packets_to_send)
+#     # print(self.packets_to_send)
+
+
+# if __name__ == "__main__":
+#     r = build_rov_packet()
+#     print(r)
+
+
+def reset_fuse_on_power_supply():
+    """reset_fuse_on_power_supply creates and adds
+    packets for resetting a fuse on the ROV"""
+    packets_to_send = []
+    fuse_reset_signal = [0] * 8
+    fuse_reset_signal[0] = 1  # set bit 0 of byte 0 to 1 to turn on the fuse
+
+    packets_to_send.append([66, fuse_reset_signal])
     print(packets_to_send)
-    # print(self.packets_to_send)
 
 
 if __name__ == "__main__":
-    r = build_rov_packet()
-    print(r)
+    print(reset_fuse_on_power_supply())
