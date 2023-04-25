f012a910e74315d0f3934f60333318c5d14e1e5c
martindimmen <martindimmen@proton.me>
2023-04-25 11:50:36 +0200

    changed values from 41,40 to 33,34

--- main.py
+++ main.py
@@ -53,10 +53,10 @@ if __name__ == "__main__":
         manual_flag = multiprocessing.Value("i", 1)
         run_gui = True
         run_craft_packet = False
-        run_network = False  # Bytt t True når du ska prøva å connecte.
+        run_network = True  # Bytt t True når du ska prøva å connecte.
         run_get_controllerdata = False
         # Sett til True om du vil sende fake sensordata til gui
-        run_send_fake_sensordata = True
+        run_send_fake_sensordata = False
 
         t_watch = Threadwatcher()
         queue_for_rov = multiprocessing.Queue()
